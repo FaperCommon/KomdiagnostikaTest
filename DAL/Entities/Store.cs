cbff0982855e81a0159d8ebe7f230aa6186e3be4
Anton <faperww@gmail.com>
2022-10-09 19:08:11 +0400

    DAL

---
+++ DAL/Entities/Store.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Entities
+{
+    public class Store : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string ISBN { get; set; }
+        public string Img { get; set; }
+        public string Desc { get; set; }
+    }
+}
