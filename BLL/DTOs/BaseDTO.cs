657687d5648bab28ddb301045a91ee8ba266b951
Anton <faperww@gmail.com>
2022-10-12 15:19:27 +0400

    BLL

---
+++ BLL/DTOs/BaseDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace BLL.DTOs
+{
+    public abstract class BaseDTO
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ BLL/DTOs/StoreDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BLL.DTOs
+{
+    public class StoreDTO : BaseDTO
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string ISBN { get; set; }
+        public string Img { get; set; }
+        public string Desc { get; set; }
+    }
+}
